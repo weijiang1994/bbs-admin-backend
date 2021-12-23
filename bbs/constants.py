a6fdd4c70c78d3645dc0d2c0fd5d05edbfed45e9
weijiang1994 <804022023@qq.com>
2021-12-23 18:17:53 +0800

    帖子相关接口

---
+++ bbs/constants.py
@@ -0,0 +1,14 @@
+"""
+# coding:utf-8
+@Time    : 2021/12/23
+@Author  : jiangwei
+@File    : constants.py
+@Desc    : constants
+@email   : qq804022023@gmail.com
+@Software: PyCharm
+"""
+NOT_FOUND = dict(
+    code=404,
+    msg='未找到相关信息！',
+    success=True
+)
